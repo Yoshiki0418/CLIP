feaaf83a6e04494c864c71857115c7c6bf3df246
Yoshiki0418 <c1206618@st.kanazawa-it.ac.jp>
2024-12-14 23:47:53 +0900

    No

--- main.py
+++ main.py
@@ -147,8 +147,7 @@ def run(args: DictConfig):
             similarity_matrix = torch.matmul(image_embeddings, text_embeddings.T)
             labels = torch.arange(similarity_matrix.size(0)).to(similarity_matrix.device)
             top_k_acc = compute_top_k_accuracy(similarity_matrix, labels, k=5)
-            val_top_k_acc.append(val_top_k_acc)
-            print(val_top_k_acc)
+            val_top_k_acc.append(top_k_acc)
 
         print(f"Epoch {epoch+1}/{args.epochs} | train loss: {np.mean(train_loss):.3f} | train top_k_acc: {np.mean(train_top_k_acc)} | val loss: {np.mean(val_loss):.3f} | val top_k_acc: {np.mean(val_top_k_acc)} ")
 
